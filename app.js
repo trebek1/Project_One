090c182161467c0b0152ddb62f9a52995b8cba4b
Alex Trzeciak <Trebek@Alexs-MacBook-Air.local>
2015-04-15 15:14:00 -0700

    Implement YouTube API

--- app.js
+++ app.js
@@ -76,37 +76,96 @@ app.get('/signup', function(req,res){
 
 
 app.get('/profile', function(req,res){
-	// fetch user's years and pass to view
-	var year = req.body; 
-	console.log(year); 
-	req.currentUser().then(function(dbUser){
-		if (dbUser) {
-			console.log(dbUser); 
-			res.render('user/profile', {Batman: dbUser.username, Year: year});
+	req.currentUser().then(function(user,box){
+		if (user) {
+			// db.Year.findAll({ include: user }, function(years) {
+			// 	res.render('user/profile', { user: user.username, years: years });
+			// });
+
+			user.getYears().then(function(years) {
+				res.render('user/profile', { username: user.dataValues.username, years: years });
+			});
 		} else {
 			res.redirect('login');
 		}
 	});
-}); 
+});
 
 
 app.post('/profile', function(req,res){
 	console.log("Hello from profile post route")
 	// create year with req.body
 
-	var year = req.body.year; 
+	var year = req.body.year;
 
-	db.Year.create({year: year}); 
+	db.Year.create({year: year, UserId: req.session.userId });
 	
+	// var box = [];
 
-	req.currentUser().then(function(dbUser){
-		res.render('user/profile', {Batman: dbUser.username, Year: year});
-		});
-	});
+	// box.push(year);
 
-// app.get('/:year', function(req, res) {
-// 	// do api call
-// 	// render view with api results
+	// console.log( "THIS IS BOX", box);
+	// req.currentUser().then(function(dbUser){
+	// 	res.render('user/profile', {Batman: dbUser.username, Year: box});
+	// });
+	res.redirect('/profile');
+});
+
+//app.get('year', function(req, res) {
+	// do api call
+	// render view with api results
+
+	//GET https://www.googleapis.com/youtube/v3/search?part=snippet&q=hot+100+number+1+hits+WATEVERYEARISSELECTED&type=playlist&key={YOUR_API_KEY} PLAYJUSTTHEFIRSTQUERY
+
+// API KEY : API key	AIzaSyDmbWcViqYeOyx0Kqsqx_Ewl0MMeZF0I3w
+// O AUTH : Client ID	673212558373-iig5j03u1j7gs1a6b1j50o1a282eikbd.apps.googleusercontent.com
+
+
+// request URL:: GET https://www.googleapis.com/youtube/v3/channels?part=contentDetails
+//                                                  &mine=true
+
+// create playlist 
+
+// POST {base_URL}/playlists?part=snippet
+//  Request body:
+//   {
+//     'snippet': {
+//       'title': 'New playlist', 
+//       'description': 'Sample playlist for Data API',
+//      }
+//   }
+
+//Add video to a playlist 
+
+// POST {base_URL}/playlistItems?part=snippet
+//   Request body:
+//   {
+//     'snippet': {
+//       'playlistId': '{PLAYLIST_ID}', 
+//       'resourceId': {
+//           'kind': 'youtube#video',
+//           'videoId': '{VIDEO_ID}'
+//         }
+//      'position': 0
+//       }
+//    }
+
+// // });
+
+
+// GET REQUEST FROM BEFORE 
+
+// app.get('/movie', function(req,res){
+// 	var imdbID = req.query.id;
+
+// 	var url = 'http://www.omdbapi.com?i='+imdbID;
+// 	request(url, function(err, resp, body){
+// 		if (!err && resp.statusCode === 200) {
+// 			var banana = JSON.parse(body);
+// 						console.log(banana);
+// 			res.render("movie", {movie: banana});	
+// 		}
+// 	});
 // });
 
 app.post('/login', function(req,res){
@@ -141,6 +200,19 @@ app.delete('/logout', function(req,res){
 	console.log('starcraft');
 	res.redirect('/');
 	console.log('starcraft II'); 
+});
+
+app.get('/years/:year', function(req, res) {
+	var year = req.params.year;
+	var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=hot+100+number+1+hits+";
+	url += year;
+	url += "&type=playlist&key=AIzaSyDmbWcViqYeOyx0Kqsqx_Ewl0MMeZF0I3w";
+
+	request(url, function(err, response, body) {
+		var results = JSON.parse(body).items;
+
+		res.render('user/year', { results: results });
+	});
 });
 
 
--- migrations/20150414165238-create-year.js
+++ migrations/20150414165238-create-year.js
@@ -9,7 +9,7 @@ module.exports = {
   		  	primaryKey: true,
   		  	type: DataTypes.INTEGER
   		},
-  		userId: {
+  		UserId: {
   			type: DataTypes.INTEGER
   		},
   		year: {
